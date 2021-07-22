4ceaae8c3cf58cf417327aaf6b9c7377958c19ed
nikhil-kumar787 <nikhilkumar7167@gmail.com>
2021-07-22 21:39:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import { SnackbarProvider } from 'notistack';
+
+import history from './Utilities/history';
+import PrivateRoute from './Utilities/private-route';
+import Home from './Home/Home';
+import Chat from './Chat/Chat';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#58a5f0',
+            main: '#0277bd',
+            dark: '#004c8c',
+        },
+        secondary: {
+            light: '#ffd95a',
+            main: '#f9a825',
+            dark: '#c17900',
+            contrastText: '#212121',
+        },
+        background: {
+            default: '#f0f0f0',
+        },
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+                <Router history={history}>
+                    <Route path="/" exact component={Home} />
+                    <PrivateRoute path="/chat" component={Chat} />
+                </Router>
+            </SnackbarProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
